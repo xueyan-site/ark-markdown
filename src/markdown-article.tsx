6fa45fff3fd909d1e38148389dbd3bb084b20043
xueyan <yang@xueyan.site>
2021-02-16 17:08:11 +0800

    fix: sync props data to provider value

--- src/markdown-article.tsx
+++ src/markdown-article.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import classNames from 'classnames'
-import { MarkdownArticleProps } from './types'
+import Store from 'xueyan-react-store'
+import { MarkdownConfig, MarkdownArticleProps } from './types'
 import { MarkdownConfigProvider } from './stores'
 import styles from './index.module.scss'
 
 export default function MarkdownArticle({ dark, darkCode, className, children, ...props }: MarkdownArticleProps) {
+  const storeRef = useRef<Store<MarkdownConfig>>(null)
+  useEffect(() => {
+    if (storeRef.current) {
+      storeRef.current.setPartData({ dark, darkCode })
+    }
+  }, [dark, darkCode])
   return (
-    <MarkdownConfigProvider dark={dark} darkCode={darkCode}>
+    <MarkdownConfigProvider ref={storeRef} dark={dark} darkCode={darkCode}>
       <article className={classNames(styles.markdown, styles.airticle, dark && styles.dark, className)} {...props}>
         {children}
       </article>
--- src/stores.ts
+++ src/stores.ts
@@ -1,17 +1,20 @@
 import Store, { createProvider, useData, useStore } from 'xueyan-react-store'
 import { MarkdownConfig } from './types'
 
-const MARKDOWN_CONFIG_STORE_TYPE = 'xueyan-markdown-config'
+const MARKDOWN_CONFIG_STORE_TYPE = 'xueyan-react-markdown-config'
 
 export const MarkdownConfigProvider = createProvider((props: MarkdownConfig) => {
-  return new Store<MarkdownConfig>(MARKDOWN_CONFIG_STORE_TYPE, props)
+  return new Store<MarkdownConfig>(MARKDOWN_CONFIG_STORE_TYPE, {
+    dark: props.dark,
+    darkCode: props.darkCode
+  })
 })
 
-export function useMarkdownConfig({ dark, darkCode }: MarkdownConfig = {}) {
+export function useMarkdownConfig(props: MarkdownConfig = {}) {
   const config = useData<MarkdownConfig>(MARKDOWN_CONFIG_STORE_TYPE)
-  const realDark = dark !== undefined ? dark : config.dark
-  const realDarkCode = darkCode !== undefined
-    ? darkCode
+  const realDark = props.dark !== undefined ? props.dark : config.dark
+  const realDarkCode = props.darkCode !== undefined
+    ? props.darkCode
     : config.darkCode !== undefined
       ? config.darkCode
       : realDark
